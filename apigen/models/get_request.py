c7b6f9a7b82f87495e2cdfc26aedb1e8e9d88a7f
yudun1989 <yudun1989@gmail.com>
2012-10-27 21:55:56 +0800

    delete method

--- apigen/models/get_request.py
+++ apigen/models/get_request.py
@@ -6,14 +6,12 @@ from apigen import db
 
 class GetRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    method = db.Column(db.String(100))
     params = db.Column(db.Text)
     resp = db.Column(db.Text)
     create_time = db.Column(db.DateTime)
     update_time = db.Column(db.DateTime)
 
-    def __init__(self, method, params, resp, create_time=None, update_time=None):
-        self.method = method
+    def __init__(self, params, resp, create_time=None, update_time=None):
         self.params = params
         self.resp = resp
         now = datetime.datetime.now()
@@ -21,4 +19,4 @@ class GetRequest(db.Model):
         self.update_time = update_time or now
 
     def __repr__(self):
-        return '<GetRequest %s %s %s>' % (self.id, self.method, self.params)
+        return '<GetRequest %s %s %s>' % (self.id, self.params)
--- apigen/views.py
+++ apigen/views.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #coding: utf8
-from flask import request, render_template, redirect
+from flask import request, render_template,\
+    redirect, abort
 from apigen import app, db
 from apigen.models.get_request import GetRequest
 from jinja2 import Template
@@ -15,14 +16,13 @@ def create():
 
 @app.route('/create', methods=['POST'])
 def create_post():
-    method = request.form['method']
     params = request.form['params']
     resp = request.form['resp']
-    if (method and params and resp):
+    if (params and resp):
         dump_result = dump_dict(params)
         if not dump_result:
             return redirect('/')
-        request_instance = GetRequest(method=method, params=json.dumps(dump_result), resp=resp)
+        request_instance = GetRequest(params=json.dumps(dump_result), resp=resp)
         db.session.add(request_instance)
         db.session.commit()
     return redirect('/')
@@ -36,11 +36,10 @@ def home():
 
 @app.route('/service/<apigen_id>')
 def apigen(apigen_id=None):
-    gr = apigen_id and db.session.query(GetRequest).filter(GetRequest.id == apigen_id).one()
+    gr = apigen_id and db.session.query(GetRequest).get(apigen_id)
     if not gr:
-        return redirect('/')
+        abort(404)
     params = json.loads(gr.params)
     result = change_dict(params, request.args)
     tem = Template(gr.resp)
     return Template.render(tem, **result)
-    # parameters including list,dict,int,string
