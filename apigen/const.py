5498104cd77304b98aba3c4d7d1c14e04f3126e8
gfreezy <gfreezy@gmail.com>
2012-10-28 10:49:44 +0800

    补全syntax

--- apigen/const.py
+++ apigen/const.py
@@ -4,10 +4,11 @@ Example:
    <p> Thanks for placing an order from {{ company }}. </p>
 
 
-{%if .. %}
+{% if .. %}
+   ...
 {% else %}
-{%endif%}
-
+   ...
+{% endif %}
 Example:
 {% if flag %}
     <p>Your warranty information will be included in the packaging.</p>
@@ -18,18 +19,75 @@ Example:
 
 
 {% for .. %}
-{%endfor%}
+   ...
+{% endfor %}
+Example:
+<ul>
+{% for item in item_list %}
+    <li>{{ item }}</li>
+{% endfor %}
+</ul>'''
 
+mako_syntax = '''${ variable_name }
+Example:
+   <p> Thanks for placing an order from ${ company }}. </p>
+
+
+% if .. :
+   ...
+% else  :
+   ...
+% endif
+Example:
+% if flag :
+    <p>Your warranty information will be included in the packaging.</p>
+% else :
+    <p>You didn't order a warranty, so you're on your own when
+    the products inevitably stop working.</p>
+% endif
+
+
+% for .. :
+   ...
+% endfor
+Example:
+<ul>
+% for item in item_list :
+    <li>{{ item }}</li>
+% endfor
+</ul>
+
+More detailed syntax, usage, please see http://www.makotemplates.org/
+'''
+
+jinja_syntax = '''{{ variable_name }}
+Example:
+   <p> Thanks for placing an order from {{ company }}. </p>
+
+
+{% if .. %}
+   ...
+{% else %}
+   ...
+{% endif %}
+Example:
+{% if flag %}
+    <p>Your warranty information will be included in the packaging.</p>
+{% else %}
+    <p>You didn't order a warranty, so you're on your own when
+    the products inevitably stop working.</p>
+{% endif %}
+
+
+{% for .. %}
+   ...
+{% endfor %}
 Example:
 <ul>
 {% for item in item_list %}
     <li>{{ item }}</li>
 {% endfor %}
 </ul>
-'''
 
-mako_syntax = '''
-'''
-
-jinja_syntax = '''
+More detailed syntax, usage, please see http://jinja.pocoo.org/docs/templates/
 '''
