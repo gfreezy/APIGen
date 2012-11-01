f4d269b3bfbb16b163bbd2585080bd237e47c6b7
gfreezy <gfreezy@gmail.com>
2012-11-01 22:50:22 +0800

    close mysql sessin after every request

--- apigen/__init__.py
+++ apigen/__init__.py
@@ -1,13 +1,33 @@
-# import sae.const
+import os
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-# from flaskext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s:%s/%s' % (sae.const.MYSQL_USER, sae.const.MYSQL_PASS, sae.const.MYSQL_HOST, sae.const.MYSQL_PORT, sae.const.MYSQL_DB)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+
+if 'SERVER_SOFTWARE' in os.environ:
+    import sae.const
+    from flaskext.sqlalchemy import SQLAlchemy
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s:%s/%s' % (
+        sae.const.MYSQL_USER, sae.const.MYSQL_PASS, sae.const.MYSQL_HOST,
+        sae.const.MYSQL_PORT, sae.const.MYSQL_DB)
+    app.config['SQLALCHEMY_POOL_RECYCLE'] = 10
+    app.debug = False
+
+else:
+    from flask.ext.sqlalchemy import SQLAlchemy
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    app.debug = True
+
 app.secret_key = 'a46bf773e4f315377d65ba1b3fe36a02'
 db = SQLAlchemy(app)
-app.debug = True
+
+
+@app.before_request
+def before_request(req):
+    db.session.close()
+    return req
+
 
 import views
