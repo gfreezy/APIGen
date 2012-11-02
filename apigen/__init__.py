e57d2d3b76463d7ff8e9047854406ce05d246cf4
gfreezy <gfreezy@gmail.com>
2012-11-02 10:36:02 +0800

    ignore

--- apigen/__init__.py
+++ apigen/__init__.py
@@ -1,3 +1,4 @@
+#coding: utf8
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -13,13 +14,13 @@ if 'SERVER_SOFTWARE' in os.environ:
         sae.const.MYSQL_USER, sae.const.MYSQL_PASS, sae.const.MYSQL_HOST,
         sae.const.MYSQL_PORT, sae.const.MYSQL_DB)
     app.config['SQLALCHEMY_POOL_RECYCLE'] = 10
-    app.debug = False
+    app.debug = True
 
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
     app.debug = True
 
-app.config['ASSETS_DEBUG'] = False
+app.config['ASSETS_DEBUG'] = app.debug
 app.secret_key = 'a46bf773e4f315377d65ba1b3fe36a02'
 db = SQLAlchemy(app)
 
--- index.wsgi
+++ index.wsgi
@@ -1,13 +1,4 @@
-import os
-import sys
 import sae
 
-app_root = os.path.dirname(__file__)
-
-# 两者取其一
-sys.path.insert(0, os.path.join(app_root, 'virtualenv.bundle'))
-
-
 from apigen import app
-
 application = sae.create_wsgi_app(app)
